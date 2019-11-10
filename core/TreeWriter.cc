f1202412fda059e0f4fb51a8773d7027669885ab
Matt Kramer <mkramer@lbl.gov>
2019-11-10 00:58:23 -0800

    Make TreeWriter movable

--- core/TreeWriter.cc
+++ core/TreeWriter.cc
@@ -7,8 +7,10 @@ template <class TreeT>          // TreeT <: TreeBase
 class TreeWriter {
 public:
   TreeWriter(const char* path, const char* title = nullptr);
+  TreeWriter(TreeWriter<TreeT>&& other);
   TreeWriter(const TreeWriter<TreeT>& other) = delete;
   ~TreeWriter();
+
   void connect(Pipeline&p, const char* outFileName = Pipeline::DefaultFile);
   void fill();
   TTree* tree() { return mgr.tree; }
@@ -27,6 +29,13 @@ TreeWriter<TreeT>::TreeWriter(const char* path, const char* title) :
 }
 
 template <class TreeT>
+TreeWriter<TreeT>::TreeWriter(TreeWriter<TreeT>&& other)
+{
+  mgr = other.mgr;
+  other.mgr.tree = nullptr;
+}
+
+template <class TreeT>
 void TreeWriter<TreeT>::connect(Pipeline& p, const char* outFileName)
 {
   TFile* f = p.getOutFile(outFileName);
@@ -39,8 +48,10 @@ void TreeWriter<TreeT>::connect(Pipeline& p, const char* outFileName)
 template <class TreeT>
 TreeWriter<TreeT>::~TreeWriter()
 {
-  mgr.tree->GetDirectory()->cd();
-  mgr.tree->Write();
+  if (mgr.tree) {
+    mgr.tree->GetDirectory()->cd();
+    mgr.tree->Write();
+  }
 }
 
 template <class TreeT>
